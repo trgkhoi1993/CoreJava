6c56e7b17c590a085eb5150f817bcdd00b5cc539
trgkhoi1993 <trgkhoi1993_uit@yahoo.com.vn>
2014-08-27 18:29:13 -0700

    dasdasdds

---
+++ Maven_CoreJava/src/main/java/model/NhanVien.java
@@ -0,0 +1,99 @@
+package model;
+
+/**
+ *
+ * @author Phan Truong Khoi
+ */
+public class NhanVien {
+    private String hoten;
+    private int heSoLuong;
+    private int giamTru;
+    private int phuCap;
+    private int luongThuong;
+    private int thue;
+    private int thunhapThue;
+    private int thucLinh;
+    private int luongCoBan;
+    private int thuNhap;
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public int getHeSoLuong() {
+        return heSoLuong;
+    }
+
+    public void setHeSoLuong(int heSoLuong) {
+        this.heSoLuong = heSoLuong;
+    }
+
+    public int getGiamTru() {
+        return giamTru;
+    }
+
+    public void setGiamTru(int giamTru) {
+        this.giamTru = giamTru;
+    }
+
+    public int getPhuCap() {
+        return phuCap;
+    }
+
+    public void setPhuCap(int phuCap) {
+        this.phuCap = phuCap;
+    }
+
+    public int getLuongThuong() {
+        return luongThuong;
+    }
+
+    public void setLuongThuong(int luongThuong) {
+        this.luongThuong = luongThuong;
+    }
+
+    public int getThue() {
+        return thue;
+    }
+
+    public void setThue(int thue) {
+        this.thue = thue;
+    }
+
+    public int getThunhapThue() {
+        return thunhapThue;
+    }
+
+    public void setThunhapThue(int thunhapThue) {
+        this.thunhapThue = thunhapThue;
+    }
+
+    public int getThucLinh() {
+        return thucLinh;
+    }
+
+    public void setThucLinh(int thucLinh) {
+        this.thucLinh = thucLinh;
+    }
+
+    public int getLuongCoBan() {
+        return luongCoBan;
+    }
+
+    public void setLuongCoBan(int luongCoBan) {
+        this.luongCoBan = luongCoBan;
+    }
+
+    public int getThuNhap() {
+        return thuNhap;
+    }
+
+    public void setThuNhap(int thuNhap) {
+        this.thuNhap = thuNhap;
+    }
+    
+}
